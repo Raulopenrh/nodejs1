36e7c124f1d6ce6e85c2660c25843c1acfd82edb
Raulopenrh <rhernandez@openrh.es>
2024-09-11 20:59:18 +0200

    se añade el paquete cors

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 // Middleware
